990871ab26328090e87e7cdf01ff46411cdb76bd
VishalFulkar <vishalfulkar9575@gmail.com>
2026-06-18 00:47:11 +0530

    Created steps components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Events from "./components/Events";
+import Steps from "./components/Steps";
+
 const App = () => {
   return (
     <div className="w-full min-h-screen">
       <Navbar />
       <Hero />
       <Events />
+      <Steps/>
     </div>
   );
 };
---
+++ src/components/Steps.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <div className='w-full min-h-screen bg-black'>
+        <div className='w-full py-6 px-4 md:py-10 md:px-8 lg:py-12 lg:px-12  flex justify-center items-center max-w-7xl mx-auto'>
+            <h2 className='text-xl justify-center items-center uppercase text-[#FF4C4C]'>Your Journey</h2>
+        </div>
+        <h1 className='orbitron-bold text-4xl sm:text-5xl md:text-6xl lg:text-[70px] text-white tracking-wider leading-[1.15]'>YOUR PATH TO THE LEAGUE</h1>
+    </div>
+  )
+}
+
+export default Steps
